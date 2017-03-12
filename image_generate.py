d82f258a85686b673295e656d93e32cb827489b7
Nick Dolzhenko <dolzhenko.nv@gmail.com>
2017-03-12 14:50:10 +0300

    First version of image_generate and alpha of words.txt

---
+++ image_generate.py
@@ -0,0 +1,56 @@
+import subprocess
+import random
+from time import sleep
+
+REQUEST_TIMEOUT = 15
+
+def cleanLine(data):
+    data.encode('ascii', 'ignore')
+    res = ''
+    for i in data:
+        if (i.isalpha() or i.isdigit() or i == ' '):
+            res += i
+    return res
+
+def isImageFromMurphy(data):
+    return 'MurphyBot' in data and 'photo' in data
+
+def getMessageNumber(data):
+    res = ''
+    pos = 0
+    while not data[pos].isdigit():
+        pos += 1
+    while data[pos].isdigit():
+        res += data[pos]
+        pos += 1
+    return res
+
+wordList = []
+
+with open("words.txt", 'r') as f:
+    for line in f:
+        wordList.extend(line.strip().split())
+
+def generateRequest():
+    word1 = random.choice(wordList)
+    word2 = random.choice(wordList)
+    return "msg MurphyBot what if " + word1 + " was " + word2 + " ?\n"
+
+print(generateRequest())
+
+tg = subprocess.Popen(["tg/bin/telegram-cli", "-CWNk", "tg/tg-server.pub"],
+                      stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=None)
+
+for line in iter(tg.stdout.readline, ''):
+    line = line.decode('UTF-8')
+    line = cleanLine(line) #delete most of the unneeded characters
+    if isImageFromMurphy(line): #if murphy sent an image download it and send a new request
+        print("got an image from Murphy!")
+        msgid = getMessageNumber(line)
+        tg.stdin.write(bytes('load_photo ' + msgid + '\n', 'UTF-8')) #image is loaded to ~/.telegram-cli/downloads
+        tg.stdin.flush()
+        sleep(REQUEST_TIMEOUT)
+        tg.stdin.write(bytes(generateRequest(), 'UTF-8'))
+        tg.stdin.flush()
+        sleep(5)
+
